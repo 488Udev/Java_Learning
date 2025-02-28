478989c0d2f03253ce75d0ec3f636380bd90407a
Dev Tech <devt5158@gmail.com>
2025-02-27 16:06:54 -0800

    files commit 4

---
+++ Methods/src/Justademo.java
@@ -0,0 +1,26 @@
+class Calculator {
+    public String addNumbers (int n, int y ) {
+
+        int Sum = n + y;
+        System.out.println(Sum);
+        if (Sum >= 100){
+            return "Sum";
+    }else {
+            return "total is less than 100";
+        }
+
+
+    }
+
+}
+public class Justademo {
+ public static void main (String[] args) {
+
+     Calculator Cal = new Calculator();
+     Cal.addNumbers(7,8);
+     System.out.println("The total is less then 100");
+
+    }
+
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
+Intro: Java is a programming language that is platform independent and can be written once and run anywhere. 
+Know the basics,learn about JVM, JRE,JDK and how Java works in general and also it's constantly upgrading so it is recommended to work with higher version of Java.
+
 This is a basic repo to practice Java and learn,feel free to clone and see the basics
+
+Feel free to make changes and practice along
